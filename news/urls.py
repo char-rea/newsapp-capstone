7ccc4fa96534de23620896b1af79de755c3b6add
Charlotte Hill <charlotte.hilll@rea-group.com>
2026-06-30 09:38:22 +0800

    docs: Add docstrings to urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,3 +1,9 @@
+"""
+urls.py
+
+URL routing configuration for the News application.
+"""
+
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
